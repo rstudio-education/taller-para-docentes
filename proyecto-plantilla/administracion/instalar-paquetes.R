fe1951cf968191e765254afe917a2098f1d8410b
Edgar Ruiz <edgar@rstudio.com>
2019-06-10 09:38:31 -0500

    Adds cheatsheets

--- proyecto-plantilla/administracion/instalar-paquetes.R
+++
@@ -1,9 +0,0 @@
-install.packages(c(
-  "ggplot2", "datos", "rmarkdown", "rprojroot", "evaluate",  
-  "digest", "highr", "markdown",   "stringr", "yaml", 
-  "Rcpp", "htmltools", "knitr", "jsonlite", "base64enc", 
-  "mime", "tidyverse", "tidymodels", "fs", "skimr", 
-  "corrr", "babynames", "fueleconomy", "nycflights13", 
-  "nasaweather", "Lahman", "gapminder", "xaringan", 
-  "flexdashboard"
-))
